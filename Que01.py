e7b8ca74ed39c243df07a04d703f01cb16f3493a
Sakshi-Belgaumkar <sakshibelgaumkar04@gmail.com>
2025-12-04 14:03:05 +0530

    save

---
+++ Que01.py
@@ -0,0 +1,18 @@
+import sys
+
+if len (sys.argv) == 5:
+    name = sys.argv[1]
+    usn = sys.argv[2]
+    department = sys.argv[3]
+    semester = sys.argv [4]
+else:
+    name = "Sakshi"
+    usn = 155
+    department = "BCA"
+    semester = "Third"
+
+    print("Student profile card")
+    print("Student name:",name)
+    print("Student USN",usn)
+    print("Student Department:",department)
+    print("Student semester:", semester)
